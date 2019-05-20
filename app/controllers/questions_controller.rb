8cb4cac1e0f21df0996ba8eae720a381f2b38742
Ricardo Prado <ricardogmprado@gmail.com>
2019-05-20 13:03:17 +0800

    form done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,20 @@
 class QuestionsController < ApplicationController
+
   def ask
   end
 
   def answer
+    @question = params[:question]
+    @answer = coach_answer(@question)
+  end
+
+  def coach_answer(message)
+    if message.include? '?'
+      'Silly question, get dressed and go to work!'
+    elsif message == 'I am going to work'
+      'Great'
+    else
+      "I don't care, get dressed and go to work!"
+    end
   end
 end
